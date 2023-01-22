7a141d82b37d8131ea80f60f8f9b12cc29f88fc5
Kamil <kamil.dmtrenko@gmail.com>
2023-01-22 20:02:36 +0100

    import components and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Footer, Navbar, RightBar, LeftBar } from "./components";
+import { LoginRegister, Home, Profile } from "./pages";
 
 import LoginRegister from "./pages/loginRegister/LoginRegister";
 
