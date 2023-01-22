394cd57cc1136641b86296a0ce656d8d3e93556f
Kamil <kamil.dmtrenko@gmail.com>
2023-01-22 20:02:13 +0100

    add index export file for components

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import Footer from "./footer/Footer";
+import Navbar from "./navbar/Navbar";
+import RightBar from "./rightBar/RightBar";
+import LeftBar from "./leftBar/LeftBar";
+
+export { Footer, Navbar, RightBar, LeftBar };
