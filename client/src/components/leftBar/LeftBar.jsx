c9ad84d82cbc090fc61e282ae383caf6919960e5
Kamil <kamil.dmtrenko@gmail.com>
2023-02-01 21:34:47 +0100

    refactor styles for left bar component

--- client/src/components/leftBar/LeftBar.jsx
+++ client/src/components/leftBar/LeftBar.jsx
@@ -15,77 +15,77 @@ import Fund from "../../assets/fundraiser.png";
 import "./leftBar.scss";
 const LeftBar = () => {
   return (
-    <aside className="aside-container">
-      <section className="menu">
-        <ul className="menu-section">
-          <li className="menu-section__user">
+    <aside className="aside-container-left">
+      <section className="menu-section">
+        <ul className="menu">
+          <li className="menu__user">
             <img
               src="https://images.pexels.com/photos/7884720/pexels-photo-7884720.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
             />
             <span>John Doe</span>
           </li>
-          <li className="menu-section__item">
+          <li className="menu__item">
             <img src={Friends} alt="" />
             <span>Friends</span>
           </li>
-          <li className="menu-section__item">
+          <li className="menu__item">
             <img src={Groups} alt="" />
             <span>Groups</span>
           </li>
-          <li className="menu-section__item">
+          <li className="menu__item">
             <img src={Market} alt="" />
             <span>Marketplace</span>
           </li>
-          <li className="menu-section__item">
+          <li className="menu__item">
             <img src={Watch} alt="" />
             <span>Watch</span>
           </li>
-          <li className="menu-section__item">
+          <li className="menu__item">
             <img src={Memories} alt="" />
             <span>Memories</span>
           </li>
         </ul>
       </section>
       <hr />
-      <section className="shortcuts">
+      <section className="shortcuts-section">
         <header className="shortcuts-section__header">Your shortcuts</header>
-        <ul className="shortcuts-section">
-          <li className="shortcuts-section__item">
+        <ul className="shortcuts">
+          <li className="shortcuts__item">
             <img src={Events} alt="" />
             <span>Events</span>
           </li>
-          <li className="shortcuts-section__item">
+          <li className="shortcuts__item">
             <img src={Gaming} alt="" />
             <span>Gaming</span>
           </li>
-          <li className="shortcuts-section__item">
+          <li className="shortcuts__item">
             <img src={Gallery} alt="" />
             <span>Gallery</span>
           </li>
-          <li className="shortcuts-section__item">
+          <li className="shortcuts__item">
             <img src={Videos} alt="" />
             <span>Videos</span>
           </li>
-          <li className="shortcuts-section__item">
+          <li className="shortcuts__item">
             <img src={Messages} alt="" />
             <span>Messages</span>
           </li>
         </ul>
       </section>
       <hr />
-      <section className="others">
+      <section className="others-section">
         <header className="others-section__header">Others</header>
-        <ul className="others-section">
-          <li className="others-section__item">
+        <ul className="others">
+          <li className="others__item">
             <img src={Fund} alt="" />
             <span>Fundraiser</span>
           </li>
-          <li className="others-section__item">
+          <li className="others__item">
             <img src={Tutorials} alt="" />
             <span>Tutorials</span>
           </li>
-          <li className="others-section__item">
+          <li className="others__item">
             <img src={Courses} alt="" />
             <span>Courses</span>
           </li>
